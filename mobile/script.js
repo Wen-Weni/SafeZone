765d96ecb1ec2d0164657e3bcd47b8bd1d55bb8f
Wen-Weni <164681846+Wen-Weni@users.noreply.github.com>
2025-09-09 07:19:29 +0300

    Add files via upload

---
+++ mobile/script.js
@@ -0,0 +1,8 @@
+
+  function toggleMenu() {
+    document.getElementById('menuOverlay').style.display = 'flex';
+  }
+
+  function closeMenu(event) {
+    document.getElementById('menuOverlay').style.display = 'none';
+  }
